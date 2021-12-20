56a00c8e17089bd5a160a889992258aab8f74ff1
ifxory-plugins <ifxandyplugins@gmail.com>
2021-12-20 20:26:42 +0300

    New Tags support

---
+++ src/main/java/de/klochk/jtag/tag/TagDouble.java
@@ -0,0 +1,61 @@
+package de.klochk.jtag.tag;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+public class TagDouble extends ITag {
+
+    @Getter
+    @Setter(value = AccessLevel.PRIVATE)
+    private double value;
+
+    public TagDouble(String name, double value) {
+
+        super(name);
+        setValue(value);
+
+    }
+
+    @Override
+    public String toString() {
+
+        final String name = getName();
+        String append = "";
+        if ((name != null) && !name.equals("")) {
+            append = "(\"" + getName() + "\")";
+        }
+        return "TAG_Double" + append + ": " + value;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+        int result = super.hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(value);
+        result = (prime * result) + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+
+        if (this == obj) { return true; }
+        if (!super.equals(obj)) { return false; }
+        if (!(obj instanceof TagDouble)) { return false; }
+        TagDouble other = (TagDouble) obj;
+        return Double.doubleToLongBits(value) == Double
+                .doubleToLongBits(other.value);
+    }
+
+}
---
+++ src/main/java/de/klochk/jtag/tag/TagEnd.java
@@ -0,0 +1,43 @@
+package de.klochk.jtag.tag;
+
+public class TagEnd extends ITag {
+
+    public TagEnd() {
+        super("");
+    }
+
+    @Override
+    public Object getValue() { return null; }
+
+    @Override
+    public String toString() {
+
+        return "TAG_End";
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result;
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+
+        if (this == obj) { return true; }
+        if (!super.equals(obj)) { return false; }
+        return obj instanceof TagEnd;
+    }
+
+}
