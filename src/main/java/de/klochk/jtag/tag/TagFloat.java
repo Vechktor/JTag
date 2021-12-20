38f4188d41b347dd0264a13e83fca5d401aca574
ifxory-plugins <ifxandyplugins@gmail.com>
2021-12-20 21:03:54 +0300

    New Tags support

---
+++ src/main/java/de/klochk/jtag/tag/TagFloat.java
@@ -0,0 +1,63 @@
+package de.klochk.jtag.tag;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <strong>TAG_FLOAT</strong>
+ */
+public class TagFloat extends ITag {
+
+    @Getter
+    @Setter(value = AccessLevel.PRIVATE)
+    private float value;
+
+    public TagFloat(String name, float value) {
+
+        super(name);
+        setValue(value);
+
+    }
+
+    @Override
+    public String toString() {
+
+        final String name = getName();
+        String append = "";
+        if ((name != null) && !name.equals("")) {
+            append = "(\"" + getName() + "\")";
+        }
+        return "TAG_Float" + append + ": " + value;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+        int result = super.hashCode();
+        result = (prime * result) + Float.floatToIntBits(value);
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+
+        if (this == obj) { return true; }
+        if (!super.equals(obj)) { return false; }
+        if (!(obj instanceof TagFloat)) { return false; }
+
+        TagFloat other = (TagFloat) obj;
+        return Float.floatToIntBits(getValue()) == Float.floatToIntBits(other.getValue());
+
+    }
+
+}
---
+++ src/main/java/de/klochk/jtag/tag/TagIntArray.java
@@ -0,0 +1,66 @@
+package de.klochk.jtag.tag;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+
+public class TagIntArray extends ITag {
+
+    @Getter
+    @Setter(value = AccessLevel.PRIVATE)
+    private int[] value;
+
+    public TagIntArray(String name, int[] array) {
+
+        super(name);
+        setValue(array);
+
+    }
+
+    @Override
+    public String toString() {
+
+        final StringBuilder integers = new StringBuilder();
+        for (final int b : value) {
+            integers.append(b).append(" ");
+        }
+        final String name = getName();
+        String append = "";
+        if ((name != null) && !name.equals("")) {
+            append = "(\"" + getName() + "\")";
+        }
+        return "TAG_Int_Array" + append + ": " + integers;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+        int result = super.hashCode();
+        result = (prime * result) + Arrays.hashCode(value);
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+
+        if (this == obj) { return true; }
+        if (!super.equals(obj)) { return false; }
+        if (!(obj instanceof TagIntArray)) { return false; }
+
+        TagIntArray other = (TagIntArray) obj;
+        return Arrays.equals(getValue(), other.getValue());
+
+    }
+
+}
