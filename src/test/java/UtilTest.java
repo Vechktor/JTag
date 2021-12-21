9f6fbc2804f821ac1c1ba1b14ff02779ab1134f6
ifxory-plugins <ifxandyplugins@gmail.com>
2021-12-21 20:54:22 +0300

    Write tests for utilities: + Generic Type Resolver test. + NBT Utility test.

---
+++ src/test/java/UtilTest.java
@@ -0,0 +1,38 @@
+import de.klochk.jtag.NBTConst;
+import de.klochk.jtag.tag.ITag;
+import de.klochk.jtag.util.GenericTypeResolver;
+import de.klochk.jtag.util.NBTUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Utility class tests
+ *
+ * @author ifxandy
+ */
+public class UtilTest {
+
+    @Test
+    @DisplayName("Generic Type Resolver")
+    public void testGenericResolver () throws NoSuchFieldException {
+
+        ITag<Integer> tagInt = new ITag<>("integer", 5);
+
+        assert(GenericTypeResolver.resolve(tagInt,
+                "value", 0).equals(Integer.class));
+
+    }
+
+    @Test
+    @DisplayName("NBTUtil")
+    public void testNBTUtil () throws NoSuchFieldException {
+
+        ITag<Integer> tagInt = new ITag<>("integer", 5);
+
+        assert(NBTUtil.getTypeId(tagInt) == NBTConst.getTypeInt());
+        assert(NBTUtil.getTypeName(tagInt).equals("TAG_INT"));
+        assert(NBTUtil.getTypeById(3).equals(Integer.class));
+
+    }
+
+}
